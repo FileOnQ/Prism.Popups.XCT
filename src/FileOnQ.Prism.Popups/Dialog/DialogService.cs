489b8fb94febbd31a81142430eb7590e0435aa68
Andrew Hoefling <andrewh@fileonq.com>
2021-06-01 19:12:34 -0400

    Add Prism dependency 7.2 which includes the dialog APIs

---
+++ src/FileOnQ.Prism.Popups/Dialog/DialogService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Prism.Services.Dialogs;
+
+namespace FileOnQ.Prism.Popups.Dialog
+{
+	class DialogService : IDialogService
+	{
+		public void ShowDialog(string name, IDialogParameters parameters, Action<IDialogResult> callback) => throw new NotImplementedException();
+	}
+}
